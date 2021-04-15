55011d62bf2001b9fd5aa84c9c0f066f7c6bf6f5
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-04-15 16:48:11 -0700

    mult

--- calc.py
+++ calc.py
@@ -1,7 +1,11 @@
 def calc(a,b):
     sum = a + b
 
-    sum = a - b
+    diff = a - b
+
+    prod = a * b
     print(
-        "Addition: ", sum
+        "Addition:", sum,
+        "Subtraction:", diff,
+        "Multiplication:", prod
     )
