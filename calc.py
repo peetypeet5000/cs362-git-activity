aa2f1c03ac660bd672237927482536f3db873e1d
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-04-15 16:49:17 -0700

    devision

--- calc.py
+++ calc.py
@@ -4,8 +4,11 @@ def calc(a,b):
     diff = a - b
 
     prod = a * b
+
+    quot = a / b
     print(
         "Addition:", sum,
         "Subtraction:", diff,
-        "Multiplication:", prod
+        "Multiplication:", prod,
+        "Devision:", quot
     )
