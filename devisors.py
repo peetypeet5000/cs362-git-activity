0756f90f6812da06dbc3eed6fb8bf0d145634102
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-04-15 17:37:25 -0700

    added user input

--- devisors.py
+++ devisors.py
@@ -1,6 +1,11 @@
-def findDevisors(a):
-    for i in range(0, a):
-        if(a % i == 0):
-            print(a, " is a devisor")
+def findDevisors(num):
+    for i in range(1, num + 1):
+        if(num % i == 0):
+            print(i, "is a devisor")
 
+
+print("enter a number: ")
+input = input()
+num = int(input)
+findDevisors(num)
     
